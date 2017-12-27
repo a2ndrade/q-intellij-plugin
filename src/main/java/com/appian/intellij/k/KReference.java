452e7f86704d1bacc8f932773e6a859e72e620d8
Antonio Andrade <antonio.andrade@appian.com>
2017-12-27 15:53:43 -0600

    Fixes ref. resolution when implicit λ params are used

--- src/main/java/com/appian/intellij/k/KReference.java
+++ src/main/java/com/appian/intellij/k/KReference.java
@@ -2,10 +2,13 @@ package com.appian.intellij.k;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 import com.appian.intellij.k.psi.KAssignment;
 import com.appian.intellij.k.psi.KLambda;
+import com.appian.intellij.k.psi.KLambdaParams;
 import com.appian.intellij.k.psi.KNamespaceDeclaration;
 import com.appian.intellij.k.psi.KUserId;
 import com.appian.intellij.k.psi.impl.KPsiImplUtil;
@@ -43,31 +46,31 @@ public final class KReference extends PsiReferenceBase<PsiElement> implements Ps
     final KUserIdCache cache = KUserIdCache.getInstance();
     final KLambda enclosingLambda = PsiTreeUtil.getContextOfType(this.myElement, KLambda.class);
     final KUserId foundInSameFile = Optional.ofNullable(enclosingLambda)
-        .map(KLambda::getLambdaParams) // 1) check the enclosing enclosingLambda params
-        .map(l -> l.getUserIdList()
+        .map(KLambda::getLambdaParams)
+        .map(KLambdaParams::getUserIdList)
+        .orElse(Collections.emptyList())
+        .stream()
+        .filter(id -> referenceName.equals(id.getName())) // 1) check the enclosing enclosingLambda params
+        .findFirst()
+        .orElse(PsiTreeUtil.findChildrenOfType(enclosingLambda, KAssignment.class) // 2) check locals
             .stream()
+            .map(KAssignment::getUserId)
             .filter(id -> referenceName.equals(id.getName()))
             .findFirst()
-            .orElse(PsiTreeUtil.findChildrenOfType(enclosingLambda, KAssignment.class) // 2) check locals
-                .stream()
-                .map(KAssignment::getUserId)
-                .filter(id -> referenceName.equals(id.getName()))
-                .findFirst()
-                .orElse(null)))
-        .orElseGet(() -> {
-          // 3) check same-file globals
-          if (KUtil.isAbsoluteId(referenceName)) {
-            return cache.findFirstExactMatch(project, sameFile, referenceName);
-          }
-          // transform relative reference into an absolute one using its current namespace
-          final String currentNs = KUtil.getCurrentNamespace(reference);
-          final String fqn = KUtil.generateFqn(currentNs, referenceName);
-          return cache.findFirstExactMatch(project, sameFile, fqn);
-        });
+            .orElseGet(() -> {
+              // 3) check same-file globals
+              if (KUtil.isAbsoluteId(referenceName)) {
+                return cache.findFirstExactMatch(project, sameFile, referenceName);
+              }
+              // transform relative reference into an absolute one using its current namespace
+              final String currentNs = KUtil.getCurrentNamespace(reference);
+              final String fqn = KUtil.generateFqn(currentNs, referenceName);
+              return cache.findFirstExactMatch(project, sameFile, fqn);
+            }));
     if (foundInSameFile != null) {
       return foundInSameFile;
     }
-    // 4) check other sameFile's globals
+    // 4) check other file's globals
     final String fqnOrName = isBuiltinQFunction(sameFile, referenceName) ?
         ".q." + referenceName : // defined under .q namespace inside q.k
         KUtil.getFqnOrName(reference);

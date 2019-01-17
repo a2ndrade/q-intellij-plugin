910a29dab580844927c0791aa1304f4e0b8161a7
YuriyYatsyk <yuriy.yatsyk@gmail.com>
2019-01-17 00:13:02 -0500

    Add icons for functions and vars in the Structure view.

--- src/main/java/com/appian/intellij/k/KIcons.java
+++ src/main/java/com/appian/intellij/k/KIcons.java
@@ -2,8 +2,18 @@ package com.appian.intellij.k;
 
 import javax.swing.*;
 
+import com.intellij.icons.AllIcons;
 import com.intellij.openapi.util.IconLoader;
+import com.intellij.ui.RowIcon;
 
 public final class KIcons {
   public static final Icon FILE = IconLoader.getIcon("q.png");
+  public static final Icon FUNCTION = IconLoader.getIcon("f.png");
+  public static final Icon VARIABLE = IconLoader.getIcon("v.png");
+
+  public static final Icon PUBLIC_FUNCTION = new RowIcon(FUNCTION, AllIcons.Nodes.C_public);
+  public static final Icon PRIVATE_FUNCTION = new RowIcon(FUNCTION, AllIcons.Nodes.C_private);
+
+  public static final Icon PUBLIC_VARIABLE = new RowIcon(VARIABLE, AllIcons.Nodes.C_public);
+  public static final Icon PRIVATE_VARIABLE = new RowIcon(VARIABLE, AllIcons.Nodes.C_private);
 }

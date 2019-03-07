376176fe08f38661bc9b0a7a94aeea8f9991f03d
Antonio Andrade <antonio.andrade@appian.com>
2019-03-06 20:05:26 -0500

    Reuses existing system icons

--- src/main/java/com/appian/intellij/k/KIcons.java
+++ src/main/java/com/appian/intellij/k/KIcons.java
@@ -1,6 +1,6 @@
 package com.appian.intellij.k;
 
-import javax.swing.*;
+import javax.swing.Icon;
 
 import com.intellij.icons.AllIcons;
 import com.intellij.openapi.util.IconLoader;
@@ -8,12 +8,10 @@ import com.intellij.ui.RowIcon;
 
 public final class KIcons {
   public static final Icon FILE = IconLoader.getIcon("q.png");
-  public static final Icon FUNCTION = IconLoader.getIcon("f.png");
-  public static final Icon VARIABLE = IconLoader.getIcon("v.png");
 
-  public static final Icon PUBLIC_FUNCTION = new RowIcon(FUNCTION, AllIcons.Nodes.C_public);
-  public static final Icon PRIVATE_FUNCTION = new RowIcon(FUNCTION, AllIcons.Nodes.C_private);
+  public static final Icon PUBLIC_FUNCTION = new RowIcon(AllIcons.Nodes.Function, AllIcons.Nodes.C_public);
+  public static final Icon PRIVATE_FUNCTION = new RowIcon(AllIcons.Nodes.Function, AllIcons.Nodes.C_private);
 
-  public static final Icon PUBLIC_VARIABLE = new RowIcon(VARIABLE, AllIcons.Nodes.C_public);
-  public static final Icon PRIVATE_VARIABLE = new RowIcon(VARIABLE, AllIcons.Nodes.C_private);
+  public static final Icon PUBLIC_VARIABLE = new RowIcon(AllIcons.Nodes.Variable, AllIcons.Nodes.C_public);
+  public static final Icon PRIVATE_VARIABLE = new RowIcon(AllIcons.Nodes.Variable, AllIcons.Nodes.C_private);
 }
